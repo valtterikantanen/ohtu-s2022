d33b7b7ac5ea75313cacd611b8eb78ca3c74f09b
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-25 00:02:33 +0200

    Kahden saman tuotteen jälkeen kori maksaa saman verran kuin tuotteen hinta tuplana

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -40,6 +40,12 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korin_hinta_on_sama_kuin_kaksi_kertaa_tuotteen_hinta(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.hinta(), 6)
+
 
 
 
